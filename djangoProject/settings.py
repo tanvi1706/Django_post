55e9c82274ae822e5b93ea33a803d58900356b77
tanvi1706 <tanvikrishnapatil@tanvis-MacBook-Pro.local>
2020-07-27 12:40:27 +0530

    okay

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'druiu+8fs749!_&u_2$(zgxosfb(&=fk4-_+l!u*!55zs6+wmb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -155,4 +156,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+#django_heroku.settings(locals())
 
