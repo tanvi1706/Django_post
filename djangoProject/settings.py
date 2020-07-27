38d69410edd220fc684b4a49d2260ab10aa0a721
tanvi1706 <tanvikrishnapatil@tanvis-MacBook-Pro.local>
2020-07-27 12:50:33 +0530

    okay, commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -158,5 +158,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
-#django_heroku.settings(locals())
+#django_heroku.settings(locals()) not required in latest version
 
