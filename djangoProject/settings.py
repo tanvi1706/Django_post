403a96a07abfd919d6d2b55da481836dba203178
tanvi1706 <tanvikrishnapatil@tanvis-MacBook-Pro.local>
2020-07-27 11:17:13 +0530

    aws setups

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'druiu+8fs749!_&u_2$(zgxosfb(&=fk4-_+l!u*!55zs6+wmb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['djangopostdemoapp.herokuapp.com']
 
 
 # Application definition
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -143,4 +146,13 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_KEY = os.environ.get('AWS_STORAGE_BUCKET_KEY')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
